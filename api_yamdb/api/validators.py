9f89f4208d2a375fb2bde266eb84531e4b7186eb
kazakov-v <kazakov.v@fxmail.ru>
2023-05-20 07:30:07 +0300

    Запрет me в любом регистре в качестве username

--- api_yamdb/api/validators.py
+++ api_yamdb/api/validators.py
@@ -2,6 +2,6 @@ from django.core.exceptions import ValidationError
 
 
 def validate_username(value):
-    if value == 'me':
+    if value.lower() == 'me':
         raise ValidationError("username не может быть 'me'")
     return value
